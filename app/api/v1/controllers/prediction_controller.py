48bfc3ed5a9955015b333af7c31b108bbef0c92f
MJackson22-bit <jm5444859@gmail.com>
2023-07-16 19:52:24 -0600

    Request image and load model to predict type disease from model `In this moment is returning array of the class, number one is the class found`

--- app/api/v1/controllers/prediction_controller.py
+++ app/api/v1/controllers/prediction_controller.py
@@ -1,9 +1,16 @@
+import io
+from PIL import Image, ImageOps
+import numpy as np
 from fastapi_utils.cbv import cbv
-from app.api.v1.schema.prediction import Prediction
 from fastapi_utils.inferring_router import InferringRouter
 from starlette.status import HTTP_200_OK
 
 from app.api.v1.services.prediction_service import PredictionService
+from fastapi import File, UploadFile
+from typing import Annotated
+import os.path
+import tensorflow as tf
+from keras.preprocessing import image
 
 prediction_router = InferringRouter()
 
@@ -12,6 +19,18 @@ prediction_router = InferringRouter()
 class PredictionController:
     prediction_service = PredictionService()
 
-    @prediction_router.get('/predictions', response_model=Prediction, status_code=HTTP_200_OK)
-    def get_prediction(self):
-        return self.prediction_service.get_prediction_image()
+    @prediction_router.post('/predictions', status_code=HTTP_200_OK)
+    def get_prediction(self, file: Annotated[bytes, File()]):
+        image_stream = io.BytesIO(file)
+        image_file = Image.open(image_stream)
+        image_resize = ImageOps.fit(image_file, (300, 300))
+        x = image.array_to_img(image_resize)
+        x = np.expand_dims(x, axis=0)
+
+        model = tf.keras.models.load_model('model_cnn/rice_leaf_diseasesrice_model.h5')
+
+        images = np.vstack([x])
+        classes = model.predict(images, batch_size=128)[0]
+
+        print(f'imageFile.size={classes}')
+        return f"classes: [{classes}]"
